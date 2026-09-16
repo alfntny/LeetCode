d9bb3181a1b97d43822fe602b2ddd0804880e723
Alfin Tony <141824681+alfntny@users.noreply.github.com>
2026-09-16 10:03:06 +0530

    Time: 22 ms (29.43%) | Memory: 19.9 MB (7.56%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        s=0
+        res=[[]]
+        for i in nums:
+            new=[]
+            for lst in res:
+                new.append(lst+[i])
+            res+=new
+        for i in res:
+            if len(i)==1:
+                s=s+i[0]
+            elif len(i)==0:
+                continue
+            else:
+                sm=0
+                for j in i:
+                    sm^=j
+                s=s+sm
+        return s
